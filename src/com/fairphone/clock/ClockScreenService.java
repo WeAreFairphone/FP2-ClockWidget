845910d5c184f857b257aec249e392ae1f89bea4
Jose Pascoal <jose.pascoal@kwamecorp.com>
2015-06-18 19:50:20 +0100

    - Added battery level logic - Layout rotation is controlled in the service to ensure that the layout does not change automatically

--- src/com/fairphone/clock/ClockScreenService.java
+++ src/com/fairphone/clock/ClockScreenService.java
@@ -7,10 +7,11 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.BatteryManager;
 import android.os.IBinder;
 import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.fairphone.clock.widget.ClockWidget;
 
@@ -22,16 +23,25 @@ public class ClockScreenService extends Service {
 	public static final String ACTION_SHARE = "com.fairphone.clock.ACTION_SHARE";
 	public static final String EXTRA_CURRENT_LAYOUT_ID = "com.fairphone.clock.EXTRA_CURRENT_LAYOUT_ID";
 	public static final String ACTION_BATTERY_SAVER = "com.fairphone.clock.ACTION_BATTERY_SAVER";
+	public static final String FAIRPHONE_CLOCK_PREFERENCES = "com.fairphone.clock.FAIRPHONE_CLOCK_PREFERENCES";
+	public static final String PREFERENCE_BATTERY_LEVEL = "com.fairphone.clock.PREFERENCE_BATTERY_LEVEL";
+	public static final String PREFERENCE_ACTIVE_LAYOUT = "com.fairphone.clock.PREFERENCE_ACTIVE_LAYOUT";
 
 	private BroadcastReceiver mRotateReceiver;
 	private BroadcastReceiver mShareReceiver;
 	private BroadcastReceiver mBatterySaverReceiver;
+	private BroadcastReceiver mBatteryStatsReceiver;
+	private SharedPreferences mSharedPreferences;
+	private static int CURRENT_LAYOUT = 0;
+
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
 		Log.wtf(TAG, "onStartCommand");
 
+
+		mSharedPreferences = getSharedPreferences(FAIRPHONE_CLOCK_PREFERENCES, MODE_PRIVATE);
 		setupLayoutRotateReceiver();
 		setupShareReceiver();
 		setupBatterySaverReceiver();
@@ -44,11 +54,17 @@ public class ClockScreenService extends Service {
 			mRotateReceiver = new BroadcastReceiver() {
 				@Override
 				public void onReceive(Context context, Intent intent) {
+					getNextActiveLayout();
 					updateWidget();
 				}
 			};
 			registerReceiver(mRotateReceiver, new IntentFilter(ACTION_ROTATE_VIEW));
 		}
+	}
+
+	private void getNextActiveLayout() {
+		CURRENT_LAYOUT++;
+		saveIntPreference(PREFERENCE_ACTIVE_LAYOUT, CURRENT_LAYOUT);
 	}
 
 	private void setupBatterySaverReceiver() {
@@ -61,6 +77,36 @@ public class ClockScreenService extends Service {
 			};
 			registerReceiver(mBatterySaverReceiver, new IntentFilter(ACTION_BATTERY_SAVER));
 		}
+
+		if (mBatteryStatsReceiver == null) {
+			mBatteryStatsReceiver = new BroadcastReceiver() {
+				@Override
+				public void onReceive(Context context, Intent intent) {
+					if (intent != null && Intent.ACTION_BATTERY_CHANGED.equals(intent.getAction())) {
+						updateBatteryLevelPreference(intent);
+
+						//only update if the current layout is the battery information
+						if(ClockWidget.getActiveLayout(mSharedPreferences) == R.id.clock_widget_battery) {
+							updateWidget();
+						}
+					}
+				}
+			};
+			registerReceiver(mBatteryStatsReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+		}
+	}
+
+	private void updateBatteryLevelPreference(Intent intent) {
+		int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+		//int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
+		Log.wtf(TAG, "Battery Level: " + level);
+		saveIntPreference(PREFERENCE_BATTERY_LEVEL, level);
+	}
+
+	private void saveIntPreference(String preference, int value) {
+		SharedPreferences.Editor editor = mSharedPreferences.edit();
+		editor.putInt(preference, value);
+		editor.commit();
 	}
 
 	private void setupShareReceiver() {
@@ -73,30 +119,24 @@ public class ClockScreenService extends Service {
 					sendIntent.setAction(Intent.ACTION_SEND);
 					sendIntent.setType("text/plain");
 
-					if(intent != null) {
-						if (intent.hasExtra(EXTRA_CURRENT_LAYOUT_ID)) {
-							int active_layout = intent.getIntExtra(EXTRA_CURRENT_LAYOUT_ID, -1);
-							String shareText = null;
-							switch (active_layout) {
-								case R.id.clock_widget_peace_of_mind:
-									shareText = "I've been in peace of mind for 172 minutes!";
-//									shareText = "I've been in peace of mind for 325 minutes! A new personal record!";
-									break;
-								case R.id.clock_widget_yours_since:
-									shareText = "My Fairphone is 2 years, 7 months, and 15 days old!";
-									break;
-								default:
-									Log.wtf(TAG, "Unknown Share button: " + active_layout);
-							}
-							if (!TextUtils.isEmpty(shareText)) {
-								sendIntent.putExtra(Intent.EXTRA_TEXT, shareText);
-								sendIntent = Intent.createChooser(sendIntent, "Share to");
-								sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-								startActivity(sendIntent);
-							}
-						}
-					} else {
-						Log.wtf(TAG, "Null intent on share receiver");
+					int active_layout = ClockWidget.getActiveLayout(mSharedPreferences);
+					String shareText = null;
+					switch (active_layout) {
+						case R.id.clock_widget_peace_of_mind:
+							shareText = "I've been in peace of mind for 172 minutes!";
+//							shareText = "I've been in peace of mind for 325 minutes! A new personal record!";
+							break;
+						case R.id.clock_widget_yours_since:
+							shareText = "My Fairphone is 2 years, 7 months, and 15 days old!";
+							break;
+						default:
+							Log.wtf(TAG, "Unknown Share button: " + active_layout);
+					}
+					if (!TextUtils.isEmpty(shareText)) {
+						sendIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+						sendIntent = Intent.createChooser(sendIntent, "Share to");
+						sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+						startActivity(sendIntent);
 					}
 				}
 			};
--- src/com/fairphone/clock/widget/ClockWidget.java
+++ src/com/fairphone/clock/widget/ClockWidget.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Build;
 import android.provider.Settings;
@@ -26,7 +27,6 @@ public class ClockWidget extends AppWidgetProvider {
     private static final String TAG = ClockWidget.class.getSimpleName();
 
     public static final int[] CLOCK_WIDGET_LAYOUTS = { R.id.clock_widget_main, R.id.clock_widget_peace_of_mind, R.id.clock_widget_battery, R.id.clock_widget_yours_since};
-    private static int CURRENT_LAYOUT = 0;
 
     private static final SecureRandom r = new SecureRandom();
 
@@ -52,25 +52,33 @@ public class ClockWidget extends AppWidgetProvider {
     }
 
     private void setupView(Context context, RemoteViews mainWidgetView) {
-        int active_layout = CLOCK_WIDGET_LAYOUTS[(CURRENT_LAYOUT++) % CLOCK_WIDGET_LAYOUTS.length];
+        SharedPreferences sharedPrefs = context.getSharedPreferences(ClockScreenService.FAIRPHONE_CLOCK_PREFERENCES, Context.MODE_PRIVATE);
+        int active_layout = getActiveLayout(sharedPrefs);
         mainWidgetView.setViewVisibility(active_layout, View.VISIBLE);
-        setupActiveView(context, mainWidgetView, active_layout);
+        setupActiveView(context, mainWidgetView, active_layout, sharedPrefs);
         setupWidgetOnClick(context, mainWidgetView, active_layout);
     }
 
-    private void setupActiveView(Context context, RemoteViews widget, int active_layout) {
+    public static int getActiveLayout(SharedPreferences sharedPrefs) {
+        int currentLayout = sharedPrefs.getInt(ClockScreenService.PREFERENCE_ACTIVE_LAYOUT, 0);
+        return CLOCK_WIDGET_LAYOUTS[(currentLayout) % CLOCK_WIDGET_LAYOUTS.length];
+    }
+
+    private void setupActiveView(Context context, RemoteViews widget, int active_layout, SharedPreferences sharedPrefs) {
         switch (active_layout){
             case R.id.clock_widget_main:
                 setNextScheduledAlarm(context, widget);
                 break;
             case R.id.clock_widget_peace_of_mind:
-                setupShareOnClick(context, widget, active_layout, R.id.peace_share_button);
+                setupShareOnClick(context, widget, R.id.peace_share_button);
                 break;
             case R.id.clock_widget_battery:
+                int batteryLevel = sharedPrefs.getInt(ClockScreenService.PREFERENCE_BATTERY_LEVEL, 0);
+                showBatteryLevel(widget, batteryLevel);
                 setupLastLongerOnClick(context, widget);
                 break;
             case R.id.clock_widget_yours_since:
-                setupShareOnClick(context, widget, active_layout, R.id.yours_since_share_button);
+                setupShareOnClick(context, widget, R.id.yours_since_share_button);
                 break;
             default:
                 Log.wtf(TAG, "Unknow layout: " + active_layout);
@@ -83,9 +91,8 @@ public class ClockWidget extends AppWidgetProvider {
         widget.setOnClickPendingIntent(R.id.last_longer_button, launchPendingIntent);
     }
 
-    private void setupShareOnClick(Context context, RemoteViews widget, int active_layout, int shareButtonId) {
+    private void setupShareOnClick(Context context, RemoteViews widget, int shareButtonId) {
         Intent launchIntent = new Intent(ClockScreenService.ACTION_SHARE);
-        launchIntent.putExtra(ClockScreenService.EXTRA_CURRENT_LAYOUT_ID, active_layout);
         PendingIntent launchPendingIntent = PendingIntent.getBroadcast(context, r.nextInt(), launchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         widget.setOnClickPendingIntent(shareButtonId, launchPendingIntent);
     }
@@ -126,5 +133,30 @@ public class ClockWidget extends AppWidgetProvider {
             nextAlarm = Settings.System.getString(context.getContentResolver(), Settings.System.NEXT_ALARM_FORMATTED);
         }
         return nextAlarm;
+    }
+
+    private void showBatteryLevel(RemoteViews widget, int batteryLevel) {
+
+        if (batteryLevel <= 10) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_10);
+        } else if (batteryLevel <= 20) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_20);
+        } else if (batteryLevel <= 30) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_30);
+        } else if (batteryLevel <= 40) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_40);
+        } else if (batteryLevel <= 50) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_50);
+        } else if (batteryLevel <= 60) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_60);
+        } else if (batteryLevel <= 70) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_70);
+        } else if (batteryLevel <= 80) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_80);
+        } else if (batteryLevel <= 90) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_90);
+        } else if (batteryLevel <= 100) {
+            widget.setImageViewResource(R.id.battery_level_image, R.drawable.battery_100);
+        }
     }
 }
