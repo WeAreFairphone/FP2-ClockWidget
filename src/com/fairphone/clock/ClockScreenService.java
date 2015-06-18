b9d9a70e3bede5185eeb3bdabe867846e69c1212
Filipe Gonçalves <filipe@kwamecorp.com>
2015-06-18 14:17:53 +0100

    * share button now shares

--- src/com/fairphone/clock/ClockScreenService.java
+++ src/com/fairphone/clock/ClockScreenService.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -67,18 +68,31 @@ public class ClockScreenService extends Service {
 			mShareReceiver = new BroadcastReceiver() {
 				@Override
 				public void onReceive(Context context, Intent intent) {
+
+					Intent sendIntent = new Intent();
+					sendIntent.setAction(Intent.ACTION_SEND);
+					sendIntent.setType("text/plain");
+
 					if(intent != null) {
 						if (intent.hasExtra(EXTRA_CURRENT_LAYOUT_ID)) {
 							int active_layout = intent.getIntExtra(EXTRA_CURRENT_LAYOUT_ID, -1);
+							String shareText = null;
 							switch (active_layout) {
 								case R.id.clock_widget_peace_of_mind:
-									Toast.makeText(context, "Share Peace of mind data", Toast.LENGTH_SHORT).show();
+									shareText = "I've been in peace of mind for 172 minutes!";
+//									shareText = "I've been in peace of mind for 325 minutes! A new personal record!";
 									break;
 								case R.id.clock_widget_yours_since:
-									Toast.makeText(context,"Share Your fairphone since data",Toast.LENGTH_SHORT).show();
+									shareText = "My Fairphone is 2 years, 7 months, and 15 days old!";
 									break;
 								default:
 									Log.wtf(TAG, "Unknown Share button: " + active_layout);
+							}
+							if (!TextUtils.isEmpty(shareText)) {
+								sendIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+								sendIntent = Intent.createChooser(sendIntent, "Share to");
+								sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								startActivity(sendIntent);
 							}
 						}
 					} else {
