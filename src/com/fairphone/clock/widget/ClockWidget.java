800b66646ba5604c54ab90eb86bcb73155f71ac1
Jose Pascoal <jose.pascoal@kwamecorp.com>
2015-06-26 13:02:03 +0100

    - Add battery remaining time end day

--- src/com/fairphone/clock/widget/ClockWidget.java
+++ src/com/fairphone/clock/widget/ClockWidget.java
@@ -263,8 +263,13 @@ public class ClockWidget extends AppWidgetProvider {
         DateTime currentTime = new DateTime();
         DateTime endtime = currentTime.plus(remainingTime);
 
+        Log.d(TAG, "\ncurrentTime" + currentTime.toString() + "\nendTime: " + endtime.toString() + "\nRemaining time: " + PeriodFormat.getDefault().withLocale(Locale.getDefault()).print(new Period(remainingTime)));
         if (endtime.getDayOfMonth() <= currentTime.getDayOfMonth() + 1) {
-            Log.d(TAG, "endTIme: " + endtime.toString() + "\nRemaining time: " + PeriodFormat.getDefault().withLocale(Locale.getDefault()).print(new Period(remainingTime)));
+            if(endtime.getDayOfMonth() != currentTime.getDayOfMonth()){
+                widget.setViewVisibility(R.id.day_indicator, View.VISIBLE);
+            }else{
+                widget.setViewVisibility(R.id.day_indicator, View.INVISIBLE);
+            }
             widget.setTextViewText(R.id.battery_description, isCharging ? resources.getString(R.string.battery_will_be_charged_at) : resources.getString(R.string.battery_charge_will_last_until));
             widget.setViewVisibility(R.id.battery_time_group, View.VISIBLE);
             widget.setViewVisibility(R.id.battery_days_left, View.GONE);
